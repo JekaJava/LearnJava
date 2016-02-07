d591ea7a452d102eb1ea2830c0043c730e0aa68d
JekaJava <wojda007@ukr.net>
2016-02-07 14:59:08 +0200

    startLearn

---
+++ src/main/java/ua/jeka_roxy/Learn.java
@@ -0,0 +1,10 @@
+package ua.jeka_roxy;
+
+/**
+ * Created by wojda on 07.02.2016.
+ */
+public class Learn {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
