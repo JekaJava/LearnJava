af88a138a4be58ce07034668ef410f05182e25a1
JekaJava <wojda007@ukr.net>
2016-02-08 01:29:35 +0200

    upgrade

--- src/main/java/ua/jeka_roxy/Learn.java
+++ src/main/java/ua/jeka_roxy/Learn.java
@@ -1,25 +1,49 @@
 package ua.jeka_roxy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Created by wojda on 07.02.2016.
  */
 public class Learn {
     public static void main(String[] args) throws IOException {
-        /*создать матрицу х * х( размер указывает пользователь )
-        заполнить рандомными числами, но значение елементов по диагонале совпадает
-        */
-        int rowSize = Integer.parseInt(args[0]);
-        int columnSize = Integer.parseInt(args[1]);
+        int rowSize, columnSize;
+        try {
+             rowSize = Integer.parseInt(args[0]);
+            columnSize = Integer.parseInt(args[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Input rowSize");
+            rowSize = Integer.parseInt(reader.readLine());
+            System.out.println("Input columnSize");
+            columnSize = Integer.parseInt(reader.readLine());
+        }
         int matrix[][] = new int[rowSize][columnSize];
 
+        Random random = new Random();
         for (int row = 0; row < rowSize; row++) {
             for (int column = 0; column < columnSize; column++) {
-                matrix[row][column] = (int)(Math.random() * 100);
-                System.out.print(matrix[row][column] + " ");
+                matrix[row][column] = random.nextInt(100);
+                System.out.print(matrix[row][column] + "\t");
             }
             System.out.println(" ");
         }
     }
 }
+/*создать матрицу х * х( размер указывает пользователь ) заполнить рандомными числами, но значение елементов по диагонале совпадает
+
+
+добивил блок try ... catch, если запускать в иде то ввод пропускаеться ( ошибка ArrayIndexOutOfBoundsException )
+если с консоли то будет работать твой вариант, а если с идеа - мой
+
+прикольная идея: ты не делала отдельно цикл для вывода, а сразу выводила - прогрес на лицо
+
+(int) (Math.random() * 100) - избавился от преобразования типа ( создал объект класса рандом, вызвал метод nextInt ( аргумент - макс число ) )
+
+насчет диагонали - это елемент у которого совпадают значение  row и column, можно заменить на заданное число \ первое чилсло кторое выпадет из рандома
+
+ сделай так, что - бы вывод был ровным ( табуляция )
+*/
