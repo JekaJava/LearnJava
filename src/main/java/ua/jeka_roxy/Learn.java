14f53acdf1e44f96f07a851f53391ae1a540347a
RoxVer <rshveda15@gmail.com>
2016-02-08 00:47:43 +0200

    Matrix: Exception не работает почему-то, у меня работал ранее в этом коде оО

--- src/main/java/ua/jeka_roxy/Learn.java
+++ src/main/java/ua/jeka_roxy/Learn.java
@@ -1,13 +1,25 @@
 package ua.jeka_roxy;
 
+import java.io.IOException;
+
 /**
  * Created by wojda on 07.02.2016.
  */
 public class Learn {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*создать матрицу х * х( размер указывает пользователь )
         заполнить рандомными числами, но значение елементов по диагонале совпадает
          */
-        int = i;
+        int rowSize = Integer.parseInt(args[0]);
+        int columnSize = Integer.parseInt(args[1]);
+        int matrix[][] = new int[rowSize][columnSize];
+
+        for (int row = 0; row < rowSize; row++) {
+            for (int column = 0; column < columnSize; column++) {
+                matrix[row][column] = (int)(Math.random() * 100);
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.println(" ");
+        }
     }
 }
