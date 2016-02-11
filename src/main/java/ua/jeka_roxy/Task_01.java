4c790bbc1f1662b396d2440fcccf44965880b4e6
JekaJava <wojda007@ukr.net>
2016-02-11 21:36:41 +0200

    upgrade

--- src/main/java/ua/jeka_roxy/Task_01.java
+++ src/main/java/ua/jeka_roxy/Task_01.java
@@ -4,6 +4,43 @@ package ua.jeka_roxy;
  * Created by wojda on 10.02.2016.
  */
 public class Task_01 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] a1 = new int[n];
+        int[] a2 = new int[n];
+        int[] a3 = new int[n];
+        int[] a4 = new int[n];
+        int[] a5 = new int[n];
+
+        for (int i = 0; i < n ; i++) {
+            a1[i] = (int) (Math.random() * 20);
+            a2[i] = (int) (Math.random() * 20);
+            a3[i] = (int) (Math.random() * 20);
+            a4[i] = (int) (Math.random() * 20);
+            a5[i] = (int) (Math.random() * 20);
+        }
+
+        System.out.println("a1: ");
+        for (int b1 : a1 ) {
+            System.out.print(b1 + "\t");
+        }
+        System.out.println("\na2: ");
+        for (int b1 : a2 ) {
+            System.out.print(b1 + "\t");
+        }
+        System.out.println("\na3: ");
+        for (int b1 : a3 ) {
+            System.out.print(b1 + "\t");
+        }
+        System.out.println("\na4: ");
+        for (int b1 : a4 ) {
+            System.out.print(b1 + "\t");
+        }
+        System.out.println("\na5: ");
+        for (int b1 : a5 ) {
+            System.out.print(b1 + "\t");
+        }
+    }
 }
 /*
 создать пять одномерных массивов
